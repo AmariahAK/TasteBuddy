6bc146d8c5e4adc984abab8a021853591a930058
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2024-08-20 04:06:51 +0300

    complete the routes

--- server/migrations/versions/639f0a7d704c_.py
+++ server/migrations/versions/639f0a7d704c_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0b50617cfa62
+Revision ID: 639f0a7d704c
 Revises: 
-Create Date: 2024-08-20 02:24:58.328289
+Create Date: 2024-08-20 03:50:32.349128
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0b50617cfa62'
+revision = '639f0a7d704c'
 down_revision = None
 branch_labels = None
 depends_on = None
